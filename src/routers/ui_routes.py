aef8cfd3300d3986572e6846b3ab207a5d0b4cf3
barreraalexander <barreraalexander93@gmail.com>
2022-12-19 16:27:20 -0500

    update to pages

--- src/routers/ui_routes.py
+++ src/routers/ui_routes.py
@@ -82,3 +82,60 @@ def inputs(request: Request, response_model=HTMLResponse):
 @router.get("/sections/input-areas/forms", status_code=status.HTTP_200_OK)
 def forms(request: Request, response_model=HTMLResponse):
     return TEMPLATES.TemplateResponse("sections/input-areas/forms.html", {"request" : request})
+
+
+
+
+
+
+
+
+@router.get("/sections/attention-catchers/alerts", status_code=status.HTTP_200_OK)
+def alerts(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/attention-catchers/alerts.html", {"request" : request})
+
+@router.get("/sections/attention-catchers/modals", status_code=status.HTTP_200_OK)
+def modals(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/attention-catchers/modals.html", {"request" : request})
+
+@router.get("/sections/attention-catchers/tooltips_popovers", status_code=status.HTTP_200_OK)
+def tooltips_popovers(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/attention-catchers/tooltips-popovers.html", {"request" : request})
+
+
+
+
+
+
+
+@router.get("/sections/elements/avatars", status_code=status.HTTP_200_OK)
+def avatars(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/avatars.html", {"request" : request})
+
+@router.get("/sections/elements/badges", status_code=status.HTTP_200_OK)
+def badges(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/badges.html", {"request" : request})
+
+@router.get("/sections/elements/breadcrumbs", status_code=status.HTTP_200_OK)
+def breadcrumbs(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/breadcrumbs.html", {"request" : request})
+
+@router.get("/sections/elements/buttons", status_code=status.HTTP_200_OK)
+def buttons(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/buttons.html", {"request" : request})
+
+@router.get("/sections/elements/dropdowns", status_code=status.HTTP_200_OK)
+def dropdowns(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/dropdowns.html", {"request" : request})
+
+@router.get("/sections/elements/progress-bars", status_code=status.HTTP_200_OK)
+def progress_bars(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/progress-bars.html", {"request" : request})
+
+@router.get("/sections/elements/toggles", status_code=status.HTTP_200_OK)
+def toggles(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/toggles.html", {"request" : request})
+
+@router.get("/sections/elements/typography", status_code=status.HTTP_200_OK)
+def typography(request: Request, response_model=HTMLResponse):
+    return TEMPLATES.TemplateResponse("sections/elements/typography.html", {"request" : request})
